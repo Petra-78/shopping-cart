e3303b4a797d23c90083011f992c74bbae79e4a1
Petra-78 <partos.petra.0305@gmail.com>
2025-10-29 17:34:21 +0100

    Add navbar, cartcontext and outlet

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,19 @@
 import { Outlet } from "react-router";
 import "./App.css";
+import NavBar from "./components/NavBar/NavBar";
+import CartContext from "./context/CartContext";
+import { useState } from "react";
 
 function App() {
-  return <Outlet />;
+  const [cart, setCart] = useState([]);
+  return (
+    <div>
+      <CartContext value={cart}>
+        <NavBar />
+        <Outlet />
+      </CartContext>
+    </div>
+  );
 }
 
 export default App;
