dba16dbf096d5e668c78d1b103e7fdb192cc0a85
Petra-78 <partos.petra.0305@gmail.com>
2025-10-29 12:31:23 +0100

    Add navigation bar with links to the other pages

---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,19 @@
+import { Link } from "react-router";
+
+export default function NavBar() {
+  return (
+    <div>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="shop">Shop</Link>
+        </li>
+        <li>
+          <Link to="/">Cart</Link>
+        </li>
+      </ul>
+    </div>
+  );
+}
