424ed240475f3cf4e9dd527f412b9236b62a5b7f
Petra-78 <partos.petra.0305@gmail.com>
2025-10-30 13:32:44 +0100

    Move book context and book provider into the same file

--- src/context/BooksContext.jsx
+++ src/context/BooksContext.jsx
@@ -1,5 +1,39 @@
+import { useEffect, useState } from "react";
 import { createContext } from "react";
 
 const BooksContext = createContext();
+
+export function BooksProvider({ children }) {
+  const [books, setBooks] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    async function fetchBooks() {
+      try {
+        const res = await fetch("https://openlibrary.org/trending/yearly.json");
+        const data = await res.json();
+
+        const englishBooks = data.works
+          .filter((book) => book.language.includes("eng"))
+          .slice(0, 100);
+
+        setBooks(englishBooks);
+      } catch (err) {
+        setError(err);
+      } finally {
+        setLoading(false);
+      }
+    }
+
+    fetchBooks();
+  }, []);
+
+  return (
+    <BooksContext.Provider value={{ books, loading, error }}>
+      {children}
+    </BooksContext.Provider>
+  );
+}
 
 export default BooksContext;
