21900ac9247fcd0425442919e6125c328b2bd971
Petra-78 <partos.petra.0305@gmail.com>
2025-10-30 12:52:01 +0100

    Add describtion fetching and display

--- src/components/BookPage/BookPage.jsx
+++ src/components/BookPage/BookPage.jsx
@@ -1,12 +1,28 @@
 import { useParams } from "react-router";
 import BooksContext from "../../context/BooksContext";
+import { useState } from "react";
 import { useContext } from "react";
 import QuantitySelector from "../quantity-selector/QuantitySelector";
+import { useEffect } from "react";
 
 export default function BookPage() {
   const { bookId } = useParams();
   const { books, loading, error } = useContext(BooksContext);
   const book = books.find((b) => b.key.split("/").pop() === bookId);
+  const [description, setDescription] = useState("");
+
+  useEffect(() => {
+    async function fetchDescribtion() {
+      try {
+        const res = await fetch(`https://openlibrary.org/works/${bookId}.json`);
+        const data = await res.json();
+        setDescription(data.description);
+      } catch (err) {
+        console.error(err);
+      }
+    }
+    fetchDescribtion();
+  }, [bookId]);
 
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Failed to load book 😢</p>;
@@ -18,6 +34,11 @@ export default function BookPage() {
         alt={book.title}
       />
       <h1>{book.title}</h1>
+      <p>
+        {typeof description === "string"
+          ? description
+          : description?.value || "No description available."}
+      </p>
       <div className="quantitySelector">
         <QuantitySelector />
         <button>Add to cart</button>
