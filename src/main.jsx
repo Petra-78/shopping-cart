3b3f909d82ae37c1c44d4eeaa5343fd7ec1013bc
Petra-78 <partos.petra.0305@gmail.com>
2025-10-30 13:34:35 +0100

    Add cart provider

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,19 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import { RouterProvider } from "react-router";
+import { RouterProvider, createBrowserRouter } from "react-router";
 import routes from "./router.jsx";
-import { createBrowserRouter } from "react-router";
-import BooksProvider from "./context/BooksProvider.jsx";
+import { BooksProvider } from "./context/BooksContext.jsx";
+import { CartProvider } from "./context/CartContext.jsx";
 
 const router = createBrowserRouter(routes);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BooksProvider>
-      <RouterProvider router={router} />
+      <CartProvider>
+        <RouterProvider router={router} />
+      </CartProvider>
     </BooksProvider>
   </StrictMode>
 );
