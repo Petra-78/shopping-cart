14f43e28f1140b91a064f603c42447a7724e5fbf
Petra-78 <partos.petra.0305@gmail.com>
2025-10-30 13:35:13 +0100

    Remove cart provider

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,12 @@
 import { Outlet } from "react-router";
 import "./App.css";
 import NavBar from "./components/NavBar/NavBar";
-import CartContext from "./context/CartContext";
-import { useState } from "react";
 
 function App() {
-  const [cart, setCart] = useState([]);
   return (
     <div>
-      <CartContext value={cart}>
-        <NavBar />
-        <Outlet />
-      </CartContext>
+      <NavBar />
+      <Outlet />
     </div>
   );
 }
