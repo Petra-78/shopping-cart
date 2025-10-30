48a673d4aece3872935b569ad3304b1554e2ed22
Petra-78 <partos.petra.0305@gmail.com>
2025-10-30 13:31:58 +0100

    Add cart provider

--- src/context/CartContext.jsx
+++ src/context/CartContext.jsx
@@ -1,5 +1,15 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 
-const CartContext = createContext([]);
+const CartContext = createContext();
+
+export function CartProvider({ children }) {
+  const [cart, setCart] = useState([]);
+
+  return (
+    <CartContext.Provider value={[cart, setCart]}>
+      {children}
+    </CartContext.Provider>
+  );
+}
 
 export default CartContext;
