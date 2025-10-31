4aa8b6f44cbef11772e7d322112deee03fa1ec8b
Petra-78 <partos.petra.0305@gmail.com>
2025-10-31 20:05:57 +0100

    Add styling and css component

--- src/components/quantity-selector/QuantitySelector.jsx
+++ src/components/quantity-selector/QuantitySelector.jsx
@@ -1,3 +1,5 @@
+import styles from "./QuantitySelector.module.css";
+
 export default function QuantitySelector({ quantity, setQuantity }) {
   const handleDecrease = () => setQuantity(Math.max(1, quantity - 1));
   const handleIncrease = () => setQuantity(Math.min(100, quantity + 1));
@@ -9,18 +11,19 @@ export default function QuantitySelector({ quantity, setQuantity }) {
   };
 
   return (
-    <div className="quantity-selector">
-      <button type="button" onClick={handleDecrease}>
+    <div className={styles.quantitySelector}>
+      <button type="button" className={styles.button} onClick={handleDecrease}>
         -
       </button>
       <input
+        className={styles.input}
         type="number"
         value={quantity}
         min="1"
         max="100"
         onChange={handleChange}
       />
-      <button type="button" onClick={handleIncrease}>
+      <button type="button" className={styles.button} onClick={handleIncrease}>
         +
       </button>
     </div>
