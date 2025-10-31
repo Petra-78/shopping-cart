6ce457f1213c4af92f938cc722422cbadda93216
Petra-78 <partos.petra.0305@gmail.com>
2025-10-31 19:38:10 +0100

    Add logo

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,11 +1,13 @@
 import { NavLink } from "react-router";
 import CartIcon from "../cart-icon/CartIcon";
 import styles from "./NavBar.module.css";
+import logo from "../../../public/favicon.png";
 
 export default function NavBar() {
   return (
     <nav className={styles.navbar}>
       <NavLink to="/" className={styles.logo}>
+        <img src={logo} alt="book logo" />
         BookHaven
       </NavLink>
       <ul className={styles.navLinks}>
