3765698b79f061d99f6fca2b34f77e5bae32cec8
Petra-78 <partos.petra.0305@gmail.com>
2025-10-31 18:11:08 +0100

    Add header style

--- src/pages/shop/Shop.jsx
+++ src/pages/shop/Shop.jsx
@@ -4,7 +4,7 @@ import styles from "./Shop.module.css";
 export default function Shop() {
   return (
     <>
-      <h1 style={styles}>Shop from our best collection</h1>
+      <h1 className={styles.title}>Shop from our best collection</h1>
       <Books />
     </>
   );
